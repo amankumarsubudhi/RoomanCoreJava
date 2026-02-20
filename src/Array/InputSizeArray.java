59654c244c9b81ffb466ff33d7dbef58c66d869b
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-02-20 11:26:21 +0530

    intial commit

---
+++ src/Array/InputSizeArray.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Scanner;
+
+public class InputSizeArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int i =  sc.nextInt();
+        int[] a = new int[i];
+        for(int j = 0;j<i;j++){
+            System.out.println("Enter Data For mark["+j+"]");
+            a[j] = sc.nextInt();
+        }
+        for(int k = 0;k<i;k++){
+            System.out.println("Data in a["+k+"] = "+a[k]);
+        }
+    }
+
+}
