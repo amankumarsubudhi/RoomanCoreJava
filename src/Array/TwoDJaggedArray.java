d6701b622cb0b1569a38acdd7fa5a7e4540cb5ac
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-02-20 15:08:06 +0530

    intial commit

--- src/Array/TwoDJaggedArray.java
+++ src/Array/TwoDJaggedArray.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Scanner;
 
-public class JaggedArray {
+public class TwoDJaggedArray {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
---
+++ src/Array/TwoDJaggedArray1.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.Scanner;
+
+public class TwoDJaggedArray1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[3][];
+        a[0] = new int[4];
+        a[1] = new int[2];
+        a[2] = new int[3];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println("Enter the value for [" + i + "] [" + j + "]");
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Output In Matrix Form");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+}
