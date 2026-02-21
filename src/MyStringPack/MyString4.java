38158eda3a3e320c34472fe7e6a2934c15be437e
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-02-21 13:12:43 +0530

    initial commit

--- src/MyStringPack/MyString4.java
+++ src/MyStringPack/MyString4.java
@@ -5,8 +5,11 @@ public class MyString4 {
     public static void main(String[] args) {
         String s1  = new String("Rooman");
         String s2  = new String("Rooman");
+        String s3  = new String("rooman");
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equalsIgnoreCase(s3));
     }
 
 
---
+++ src/MyStringPack/MyStringMethods.java
@@ -0,0 +1,13 @@
+package MyStringPack;
+
+public class MyStringMethods {
+
+    public static void main(String[] args) {
+        String s1 = "Rooman";
+        String s2 = "ROOMAN";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s2.toLowerCase());
+    }
+
+
+}
