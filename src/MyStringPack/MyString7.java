2793d54f390cca5afde02af054a589376c6ca72c
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-02-23 15:11:58 +0530

    initial commit

---
+++ src/MyStringPack/MyString7.java
@@ -0,0 +1,20 @@
+package MyStringPack;
+
+public class MyString7 {
+
+
+    public static void main(String[] args) {
+        String s = "Odisha";
+        char[] ch = s.toCharArray();
+        System.out.println(ch[3]);
+        System.out.println(" ");
+
+        for(char c : ch){
+            System.out.println(c);
+        }
+    }
+
+
+
+
+}
