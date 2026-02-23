615b1f22baa24443c4ffe2d1c0d79bb7265c737d
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-02-23 16:01:18 +0530

    initial commit

---
+++ src/MyStringPack/MyString9.java
@@ -0,0 +1,16 @@
+package MyStringPack;
+
+public class MyString9 {
+
+
+    public static void main(String[] args) {
+        String s = "Rooman Technology Pvt Ltd";
+        System.out.println(s.charAt(6));
+        System.out.println(s.substring(6,7));
+        System.out.println(s.contains("techno"));
+        System.out.println(s.contains("Techno"));
+    }
+
+
+
+}
