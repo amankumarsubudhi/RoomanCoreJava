634f5c11759fd4dc405e4a7793422cf80a0b42b0
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-02-23 14:33:52 +0530

    updating

---
+++ src/MyStringPack/MyString5.java
@@ -0,0 +1,15 @@
+package MyStringPack;
+
+public class MyString5 {
+
+    public static void main(String[] args) {
+        String s1 = "Odisha";
+        System.out.println(s1.startsWith("Odi"));
+        System.out.println(s1.startsWith("odi"));
+        System.out.println(s1.endsWith("Sha"));
+        System.out.println(s1.endsWith("sha"));
+    }
+
+
+
+}
