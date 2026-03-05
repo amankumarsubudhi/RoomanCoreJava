6cc564ce92bb825ebdb8d5ba9c0c93bc2f5746c7
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-05 13:03:08 +0530

    trying different access modifiers

--- src/A/Abc.java
+++ src/A/Abc.java
@@ -35,7 +35,7 @@ class Efg extends Abc{
     @Override
     public void m1(){
         // showing error because it marked as private
-        System.out.println(a);
+        //System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
