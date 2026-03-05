658559ba261c6bbc029476cfa35d635cf3ef57e8
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-05 13:03:49 +0530

    trying different access modifiers

--- src/C/Xyz.java
+++ src/C/Xyz.java
@@ -7,13 +7,13 @@ public class Xyz{
         Abc b = new Abc();
 
         // showing error because it marked as private
-        System.out.println(b.a);
+        //System.out.println(b.a);
 
         // showing error because it marked as (package / default)
-        System.out.println(b.b);
+        //System.out.println(b.b);
 
         // showing error because it marked as protected
-        System.out.println(b.c);
+        //System.out.println(b.c);
 
 
         System.out.println(b.d);
