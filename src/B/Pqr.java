6e627bb964d8e6efb8114ccee9803cc52408dd88
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-05 13:03:27 +0530

    trying different access modifiers

--- src/B/Pqr.java
+++ src/B/Pqr.java
@@ -7,10 +7,10 @@ public class Pqr extends Abc {
     @Override
     public void m1(){
         // showing error because it marked as private
-        System.out.println(a);
+        //System.out.println(a);
 
         // showing error because it marked as (package / default)
-        System.out.println(b);
+        //System.out.println(b);
 
 
         System.out.println(c);
