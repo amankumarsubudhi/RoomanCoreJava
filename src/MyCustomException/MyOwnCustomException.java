611c1f2bdb694a9d49daac279e95d8706315acb5
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-19 15:58:17 +0530

    implemented my custom exception

--- src/MyCustomException/MyOwnCustomException.java
+++ src/MyCustomException/MyOwnCustomException.java
@@ -27,11 +27,44 @@ class MyLicence{
 }
 
 
+
+
+class InsufficientBalanceException extends Exception{
+    public InsufficientBalanceException(String message){
+        super(message);
+    }
+}
+
+
+class withdraw{
+     public void withdrawMoney() throws InsufficientBalanceException{
+         int balance = 85867;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Amount You Want to Withdraw");
+        int wb = sc.nextInt();
+        if (wb > balance){
+            System.out.println("Insufficient Balance");
+        }
+        else{
+            System.out.println("Withdraw " + wb + " Successful");
+        }
+    }
+}
+
+
+
+
 public class MyOwnCustomException {
 
     public static void main(String[] args) {
 
         MyLicence ml = new MyLicence();
+        withdraw w = new withdraw();
+        try {
+            w.withdrawMoney();
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
         try {
             ml.applyLicence();
         } catch (UnderAgeException e) {
