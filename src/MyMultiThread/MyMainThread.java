011885f808e29deef66d70c3255686ab0f61f21f
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-24 12:41:03 +0530

    printing current thread

---
+++ src/MyMultiThread/MyMainThread.java
@@ -0,0 +1,13 @@
+package MyMultiThread;
+
+public class MyMainThread {
+
+    public static void main(String[] args) {
+
+        Thread th1 = Thread.currentThread();
+        System.out.println(th1);
+
+
+    }
+
+}
